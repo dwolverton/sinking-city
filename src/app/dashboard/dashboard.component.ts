3088a7c4d4d78aca78c8a4edea5fa3225cb3466f
David Wolverton <dwolvert@gmail.com>
2019-10-31 15:29:39 -0400

    display captured treasures

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import BoardState from 'src/game/BoardState';
-import { TreasureCard, FloodCard, FLOOD_CARDS, TREASURE_CARDS, WATER_LEVELS } from 'src/game/boardElements';
+import { TreasureCard, FloodCard, FLOOD_CARDS, TREASURE_CARDS, WATER_LEVELS, TREASURES } from 'src/game/boardElements';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,18 +14,24 @@ export class DashboardComponent {
   treasureDiscard:TreasureCard;
   floodDiscard:FloodCard;
   waterLevel:number;
+  treasures:any;
 
   constructor() { }
 
   ngOnChanges({board:boardChg}:SimpleChanges) {
     if (boardChg) {
-      const { previousValue, currentValue } = boardChg;
+      const { previousValue, currentValue }:{ previousValue:BoardState, currentValue:BoardState } = boardChg;
       this.waterLevel = WATER_LEVELS[currentValue.waterLevel];
       if (!previousValue || currentValue.treasureDiscard !== previousValue.treasureDiscard) {
         this.treasureDiscard = getTopCard(currentValue.treasureDiscard, TREASURE_CARDS);
       }
       if (!previousValue || currentValue.floodDiscard !== previousValue.floodDiscard) {
         this.floodDiscard = getTopCard(currentValue.floodDiscard, FLOOD_CARDS);
+      }
+      if (!previousValue || currentValue.treasuresCaptured !== previousValue.treasuresCaptured) {
+        this.treasures = currentValue.treasuresCaptured.map((captured, i) => ({
+          ... TREASURES[i], captured
+        }));
       }
     }
   }
